7d1626d35c864805c700298755f646ac0219608b
Yusuke Shibata <yusukeshib@gmail.com>
2026-06-16 07:08:10 -0400

    build: sync flake.nix version to 0.6.3

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        version = "0.6.2";
+        version = "0.6.3";
       in
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
